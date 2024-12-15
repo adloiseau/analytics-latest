1fd472fa52046f162a2e1367609cba410034c083
adloiseau <adloiseau@gmail.com>
2024-12-15 14:42:13 +0100

    Update src/components/Tooltip.tsx [BoltSync]

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+interface TooltipProps {
+  content: string;
+  children: React.ReactNode;
+}
+
+export const Tooltip: React.FC<TooltipProps> = ({ content, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <div
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        {children}
+      </div>
+      {isVisible && (
+        <div className="absolute z-50 px-2 py-1 text-xs text-white bg-gray-900 rounded shadow-lg -top-8 left-1/2 transform -translate-x-1/2 whitespace-nowrap">
+          {content}
+          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 rotate-45 w-2 h-2 bg-gray-900" />
+        </div>
+      )}
+    </div>
+  );
+};
